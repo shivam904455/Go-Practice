b0e46f61ffeeb2a427fae1a82db62f07dcc4cbb1
shivam904455 <shivamrajpoot904455@gmail.com>
2023-03-31 14:48:53 +0530

    methods importexport

--- importexecutable/cmd/main.go
+++ importexecutable/cmd/main.go
@@ -4,14 +4,23 @@ import (
 	"fmt"
 
 	"github.com/shivam904455/Go-Practice/importexecutable/calculater"
+	"github.com/shivam904455/Go-Practice/importexecutable/store"
 )
 
 func main() {
-usr:=calculater.User{}
-usr.Name ="fhfsdfhs"
-	fmt.Println("sum =", calculater.Add(54646, 5464))
+
+	usr := calculater.User{}
+	usr.Name = "fhfsdfhs"
+	// fmt.Println("sum =", calculater.Add(54646, 5464))
 	fmt.Println("Sub =", calculater.Sub(54646, 5464))
 	fmt.Println("Mul =", calculater.Mul(54646, 5464))
 	fmt.Println("Div =", calculater.Div(54646, 5464))
+
+	// import using interface
+	var dboprs store.DBOperations
+
+	dboprs = store.Store{}
+
+	dboprs.SaveRecord("just print this as a record.....")
 
 }
---
+++ importexecutable/store/store.go
@@ -0,0 +1,6 @@
+package store
+
+type DBOperations interface{
+
+ SaveRecord(record interface{})
+}
